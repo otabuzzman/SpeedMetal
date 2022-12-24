a6cb76de77975f987ea1c3ac79dac021d9b1c8f0
Jürgen Schuck <otabuzzman@users.noreply.github.com>
2022-12-24 13:42:48 +0100

    Rework code for Mac&i article

--- Sources/SpeedMetal/Stage.swift
+++ Sources/SpeedMetal/Stage.swift
@@ -188,12 +188,12 @@ class TriangleGeometry: Geometry {
         }
 
         for triangleIndex in 0..<2 {
-            var n = [MTLPackedFloat3]()
-            var c = [MTLPackedFloat3]()
+            var n = [vector_float3]()
+            var c = [vector_float3]()
             for i in 0..<3 {
                 let index = Int(indices[firstIndex + triangleIndex * 3 + i])
-                n.append(MTLPackedFloat3(normals[index]))
-                c.append(MTLPackedFloat3(colors[index]))
+                n.append(normals[index])
+                c.append(colors[index])
             }
             let triangle = Triangle(normals: (n[0], n[1], n[2]), colors: (c[0], c[1], c[2]))
             triangles.append(triangle)
@@ -385,9 +385,9 @@ class Stage {
                 mask: GEOMETRY_MASK_SPHERE)
             stage.addGeometryInstance(instance: sphereGeometryInstance)
             
-            let r = Float.random(in: 0.0..<1.0)
-            let g = Float.random(in: 0.0..<1.0)
-            let b = Float.random(in: 0.0..<1.0)
+            let r = Float.random(in: 0.0...1.0)
+            let g = Float.random(in: 0.0...1.0)
+            let b = Float.random(in: 0.0...1.0)
             
             let areaLight = AreaLight(
                 position: vector_float3(x * 2.5, y * 2.5 + 1.98, 0.0),
